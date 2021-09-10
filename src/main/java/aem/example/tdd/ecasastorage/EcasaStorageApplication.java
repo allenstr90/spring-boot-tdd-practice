4f37773592a64c4f7460abe4a0fc083c23b9acbf
allenstr90 <allenstr90@gmail.com>
2021-09-09 23:02:42 -0400

    feat: initial commit

---
+++ src/main/java/aem/example/tdd/ecasastorage/EcasaStorageApplication.java
@@ -0,0 +1,13 @@
+package aem.example.tdd.ecasastorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcasaStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcasaStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aem/example/tdd/ecasastorage/EcasaStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package aem.example.tdd.ecasastorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcasaStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
